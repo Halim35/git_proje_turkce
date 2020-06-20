6d39d162bb618dcabda584083301627648030b7b
Halim35 <halimaltinisik74@gmail.com>
2020-06-20 20:35:49 +0200

    ikinci commit local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,7 +4,10 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
-
+		
+		System.out.println("Second commit from local repository");
+		
+		
 	}
 
 }
